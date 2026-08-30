08c28718151d91ba7b1f9b0c01714197c2e58166
smrha <lord.smrha@gmail.com>
2026-08-30 14:44:38 +0330

    [ADD] dashboard view

--- src/urls.py
+++ src/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from accounts import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('dashboard', views.dashboard, name='dashboard'),
     path('accounts/', include('accounts.urls')),
 ]
